f9deb20f43b837b519220df7eac1c1107294b3b3
Dmytro Kozlovskyi <dnfdim@gmail.com>
2023-03-14 23:07:51 +0200

    +

---
+++ js/script.js
@@ -0,0 +1,17 @@
+console.log (Number(true+true)); 
+
+console.log (0.1+0.3)
+
+console.log (0.1+0.3==0.4)
+
+
+
+const numb1 = 0.3 
+const numb2 = 0.6
+const sum1 =numb1 + numb2
+console.log (numb1+numb2)
+
+console.log (Number (sum1.toFixed(3)))
+
+
+console.log (1 +" 2")
