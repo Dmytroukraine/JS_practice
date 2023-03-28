2c09a0343b13993ac0e4f081c38e8d6231f37019
Dmytro Kozlovskyi <dnfdim@gmail.com>
2023-03-28 23:22:17 +0300

    +

--- js/script.js
+++ js/script.js
@@ -235,3 +235,33 @@
 // let value = 5;
 // value -= 10;
 // console.log(value);
+
+
+
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   genres: ["historical prose", "adventure"],
+//   isPublic: true,
+//   rating: 8.38,
+// };
+
+// book.pageCount = 836;
+// book.originalLanguage = "en";
+// book.translations = ["ua", "ru"];
+
+// console.log(book);
+
+const book = {
+  title: "The Last Kingdom",
+  author: "Bernard Cornwell",
+  genres: ["historical prose", "adventure"],
+  rating: 8.38,
+};
+
+for (const key in book) {
+  // Ключ
+  console.log(key);
+  // Значення властивості з таким ключем
+  console.log(book[key]);
+}
