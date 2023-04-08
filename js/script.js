45e41d64b5e6f050c20516d4074910166a6194d2
Dmytro Kozlovskyi <dnfdim@gmail.com>
2023-04-08 13:16:01 +0300

    +

--- js/script.js
+++ js/script.js
@@ -581,6 +581,130 @@
 //   console.log(`Індекс ${index}, значення ${number}`);
 // });
 
-setTimeout(function() {
-  console.log('Цей код виконується через 2 секунди');
-}, 2000);
+// setTimeout(function() {
+//   console.log('Цей код виконується через 2 секунди');
+// }, 2000);
+
+
+
+// const makeShef = function (shefName) {
+//   return function (dish) {
+//     console.log(`${shefName} is cooking ${dish}`);
+//   };
+// };
+
+
+// const chefJohn = makeShef('John');
+// const chefMary = makeShef('Mary');
+
+// chefJohn('spaghetti');
+// chefMary('lasagna');
+
+
+const vehicles = [
+  {
+    make: "Honda",
+    model: "CR-V",
+    type: "suv",
+    amount: 14,
+    price: 24045,
+    onSale: true,
+  },
+  {
+    make: "Honda",
+    model: "Accord",
+    type: "sedan",
+    amount: 2,
+    price: 22455,
+    onSale: true,
+  },
+  {
+    make: "Mazda",
+    model: "Mazda 6",
+    type: "sedan",
+    amount: 8,
+    price: 24195,
+    onSale: false,
+  },
+  {
+    make: "Mazda",
+    model: "CX-9",
+    type: "suv",
+    amount: 7,
+    price: 31520,
+    onSale: true,
+  },
+{
+    make: "Toyota",
+    model: "4Runner",
+    type: "suv",
+    amount: 19,
+    price: 34210,
+    onSale: false,
+  },
+  {
+    make: "Toyota",
+    model: "Sequoia",
+    type: "suv",
+    amount: 16,
+    price: 45560,
+    onSale: false,
+  },
+  {
+    make: "Toyota",
+    model: "Tacoma",
+    type: "truck",
+    amount: 4,
+    price: 24320,
+    onSale: true,
+  },
+  {
+    make: "Ford",
+    model: "F-150",
+    type: "truck",
+    amount: 11,
+    price: 27110,
+    onSale: true,
+  },
+  {
+    make: "Ford",
+    model: "Fusion",
+    type: "sedan",
+    amount: 13,
+    price: 22120,
+    onSale: true,
+  },
+  {
+    make: "Ford",
+    model: "Explorer",
+    type: "suv",
+    amount: 6,
+    price: 31660,
+    onSale: false,
+  },
+];
+
+// const getModelsByAmount = function (vehicles, amount) {
+//   const models = [];
+//   for (const vehicle of vehicles) {
+//     if (vehicle.amount > amount) {
+//       models.push(vehicle.model);
+//     }
+//   }
+//   return models;
+// };
+
+// // Приклад використання:
+
+// const models = getModelsByAmount(vehicles, 15);
+// console.log(models); 
+
+
+
+function getModelsByAmount(amount) {
+  return vehicles
+    .filter(vehicle => vehicle.amount > amount)
+    .map(vehicle => vehicle.model);
+}
+
+console.log(getModelsByAmount(5));
